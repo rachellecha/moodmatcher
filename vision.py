7ff14f56d13655a1ababbb7974ef18c13bc9758d
Aidan Andrucyk <58194034+aidanandrucyk@users.noreply.github.com>
2020-09-12 00:29:30 -0400

    Pairing with Google Vision API

--- vision.py
+++ vision.py
@@ -1,7 +1,37 @@
+import io 
+import os 
+
+from google.cloud import vision 
+from google.cloud.vision import types
+
+# set the os GCP APP varibale
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'VisionAPI\moodmatchervision.json'
+
+#client for image annotate vision 
+client = vision.ImageAnnotatorClient()
+
+# image to send
+file_name = os.path.relpath('test.jpg')
+
+#reading in the image file to memory
+with io.open(file_name,'rb') as image_file:
+    content = image_file.read()
+
+# setting the image 
+image = types.Image(content=content)
+
+# making the request to vision 
+response = client.label_detection(image=image)
+
+labels = response.label_annotations
+
+for label in labels:
+    print(label)
+
+'''
 def detect_faces(path):
     """Detects faces in an image."""
-    from google.cloud import vision
-    import io
+
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
@@ -13,8 +43,7 @@ def detect_faces(path):
     faces = response.face_annotations
 
     # Names of likelihood from google.cloud.vision.enums
-    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-                       'LIKELY', 'VERY_LIKELY')
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY')
     print('Faces:')
 
     for face in faces:
@@ -32,3 +61,4 @@ def detect_faces(path):
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
+'''
