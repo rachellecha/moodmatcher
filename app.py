06df02b5461b10d2e887c12b26636738afa95e46
Rachelle <rachellecha@users.noreply.github.com>
2020-09-12 18:59:21 -0400

    half connected spotify with everything else

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ from flask import Flask, render_template, redirect, request
 #image uri to png
 from binascii import a2b_base64
 
+
+import requests
+import datetime
+from urllib.parse import urlencode
+
+
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+import json
+
+
 ## BEGIN APPLICATION ##
 
 # creates application
@@ -163,6 +176,40 @@ def feedback():
     new_d = {str(key): float(value) for key, value in keys_values}
 
     emot = max(new_d, key=new_d.get)
+
+    # get username from terminal
+    username = "12171678313"
+
+    #12171678313
+
+    #Erase cache and prompt for user permission
+
+    try:
+        token = util.prompt_for_user_token(username)
+    except:
+        #os.remove(".cache-{}".format(username))
+        token = token = util.prompt_for_user_token(username, client_id='9d5dc983184743aa992c6a6b81fa44b5', client_secret='b7f6d9fa451e406a98eb541247a63de6', redirect_uri='https://google.com/')
+
+    #create spotify object
+    spotifyObject = spotipy.Spotify(auth=token)
+
+    playlist = ''
+
+    if emot == 'joy':
+        playlist = '37i9dQZF1DXdPec7aLTmlC'
+    elif emot == 'sorrow':
+        playlist = '37i9dQZF1DWSqBruwoIXkA'
+    elif emot == 'anger':
+        playlist = '37i9dQZF1DXcfZ6moR6J0G'
+    else:
+        playlist = '37i9dQZF1DX7HOk71GPfSw'
+
+    cover = spotifyObject.playlist_cover_image(playlist)
+    playlist_cover = cover[0]['url']
+
+    playlist_url = spotifyObject.playlist(playlist)
+
+    url = playlist_url['external_urls']['spotify']
     
     return render_template("feedback.html", emot=emot)
 
